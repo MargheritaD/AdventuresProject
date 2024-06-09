10ae382dfefc0590cf4cf0321067a586295d2611
margheritadi <margherita.di@edu.uah.es>
2024-06-09 19:44:39 +0200

    commit 223344

--- src/main/java/com/example/adventures/bean/ItineraryStopBean.java
+++ src/main/java/com/example/adventures/bean/ItineraryStopBean.java
@@ -3,14 +3,11 @@ package com.example.adventures.bean;
 import java.time.LocalDate;
 
 public class ItineraryStopBean {
-
-    //private int idViaggio;
     private String city;
     private LocalDate startDate;
     private LocalDate endDate;
 
     public ItineraryStopBean(String city, LocalDate startDate, LocalDate endDate) {
-        //this.idViaggio = idViaggio;
         this.city = city;
         this.startDate = startDate;
         this.endDate = endDate;
--- src/main/java/com/example/adventures/bean/TripBean.java
+++ src/main/java/com/example/adventures/bean/TripBean.java
@@ -1,7 +1,5 @@
 package com.example.adventures.bean;
 
-
-import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
