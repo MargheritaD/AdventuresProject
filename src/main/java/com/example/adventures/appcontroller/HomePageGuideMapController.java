7ced941bf3d0fe42962db3a360b3b493504ba800
margheritadi <margherita.di@edu.uah.es>
2024-06-09 20:01:47 +0200

    commit 223344556

--- src/main/java/com/example/adventures/appcontroller/HomePageGuideMapController.java
+++ src/main/java/com/example/adventures/appcontroller/HomePageGuideMapController.java
@@ -4,10 +4,14 @@ import com.example.adventures.dao.TripDAO;
 
 public class HomePageGuideMapController {
 
+    /*
+
     public int numberOfTrps(String country){
 
         int number = TripDAO.getNumberTripsByCountry(country);
 
         return number;
     }
+
+     */
 }
