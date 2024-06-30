a093b8e308604f239d087708350d062d29b0cb6d
margheritadi <margherita.di@edu.uah.es>
2024-06-30 15:33:55 +0200

    1122334455

--- src/main/java/com/example/adventures/model/Traveler.java
+++ src/main/java/com/example/adventures/model/Traveler.java
@@ -1,35 +1,32 @@
 package com.example.adventures.model;
 
 public class Traveler extends GenericUser{
-    private int Id_viaggiatore;
-    private static String Nome_traveler;
-    private String Cognome_traveler;
-    private String Email;
+    private int idViaggiatore;
+    private static String nomeViaggiatore;
+    private String cognomeViaggiatore;
+    private String email;
 
     public Traveler() {}
-    public Traveler(int Id_traveler, String Nome_traveler, String Cognome_traveler, String Email){
-        //super(Id_traveler, null, email);
-        this.Id_viaggiatore = Id_traveler;
-        this.Nome_traveler = Nome_traveler;
-        this.Cognome_traveler = Cognome_traveler;
-        this.Email = Email;
+    public Traveler(int Id_traveler, String nomeViaggiatore, String cognomeViaggiatore, String email){
+        this.idViaggiatore = Id_traveler;
+        this.nomeViaggiatore = nomeViaggiatore;
+        this.cognomeViaggiatore = cognomeViaggiatore;
+        this.email = email;
     }
 
     public static String getName() {
-        System.out.println(Nome_traveler);
-        return Nome_traveler;
+        return nomeViaggiatore;
     }
 
     public int getId(){
-        return Id_viaggiatore;
+        return idViaggiatore;
     }
 
     public String getSurname() {
-        return Cognome_traveler;
+        return cognomeViaggiatore;
     }
 
     public String getEmail(){
-        System.out.println("\nEMAIL BEN: " + Email);
-        return Email;}
+        return email;}
 
 }
