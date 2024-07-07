2a182ee018786640c580b1d9b77b9c58c20c76a1
margheritadi <margherita.di@edu.uah.es>
2024-07-07 17:55:19 +0200

    111112233334

--- src/main/java/com/example/adventures/graphiccontroller/HomePageGuideMapGUIController.java
+++ src/main/java/com/example/adventures/graphiccontroller/HomePageGuideMapGUIController.java
@@ -47,14 +47,6 @@ public class HomePageGuideMapGUIController {
 
 
 
-/*
-    private int numberOfTrps(String country){
-
-        //int number = TripDAO.getNumberTripsByCountry(country);
-
-        return number;
-    }*/
-
     public void initialize(){
 
         GuideBean guideBean = Session.getCurrentSession().getGuideBean();
@@ -62,60 +54,27 @@ public class HomePageGuideMapGUIController {
         nameLabel.setText(guideBean.getName());
         BookTripController bookTripController = new BookTripController();
         boolean bell = bookTripController.bell(guideBean);
-        System.out.println("TRUE OR FALSE: "+bell);
+
 
         if(bell){
             URL resource = getClass().getResource("/com/example/adventures/image/yellowBell.png");
             if (resource != null) {
                 String imageUrl = resource.toExternalForm();
-                System.out.println("Dopo string url\n");
-                System.out.println("URL " + imageUrl);
                 Image newImage = new Image(imageUrl);
                 bellImage.setImage(newImage);
             } else {
-                System.out.println("Immagine non trovata: /image/yellowBell.png");
+                //immagine non trovata
             }
         }else {
             URL resource = getClass().getResource("/com/example/adventures/image/emptyBell.png");
             if (resource != null) {
                 String imageUrl = resource.toExternalForm();
-                System.out.println("Dopo string url\n");
-                System.out.println("URL " + imageUrl);
                 Image newImage = new Image(imageUrl);
                 bellImage.setImage(newImage);
             } else {
-                System.out.println("Immagine non trovata: /image/emptyBell.png");
+                //immagine non trovata
             }
         }
-
-
-
-
-       /* BookTripController bookTripController = new BookTripController();
-        List <RequestBean> requestList = bookTripController.tableRequestsGuide(guideBean);
-
-        if(){
-        }
-
-            CI VA IL CONTROLLO PER VEDERE SE CI SONO NUOVE RICHIESTE.
-            SI -> YELLOWBELL -> PULSANTE ATTIVO
-            NO -> EMPTYBELL  -> PULSANTE NON ATTIVO
-         */
-
-        /*
-
-        URL resource = getClass().getResource("/com/example/adventures/image/emptyBell.png");
-        if (resource != null) {
-            String imageUrl = resource.toExternalForm();
-            System.out.println("Dopo string url\n");
-            System.out.println("URL " + imageUrl);
-            Image newImage = new Image(imageUrl);
-            bellImage.setImage(newImage);
-        } else {
-            System.out.println("Immagine non trovata: /image/yellowBell.png");
-        }
-
-         */
     }
 
     public void toRequestButton() throws IOException, NotFoundException{
@@ -124,13 +83,6 @@ public class HomePageGuideMapGUIController {
         Parent root = loader.load();
 
         UpcomingTripsGUIController upcomingTripsGUIController = loader.getController();
-        //relaxGUIController.setTitle(category);
-
-        //relaxGUIController.setCountry(country);
-
-// qui
-
-        // upcomingTripsGUIController.initialize();
         upcomingTripsGUIController.inizio();
 
         Stage dialog = Main.getStage();
@@ -138,127 +90,65 @@ public class HomePageGuideMapGUIController {
         dialog.setScene(scene);
         dialog.show();
     }
-    public void australiaMouseEntered(){
+
+    private void handleMouseEntered(String country, Label label) {
 
         BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Australia");
+        int num = quoteController.numberOfTrps(country);
         if(num == 1){
-            australiaLabel.setText("Australia: "+ num + " avaiable trip");
+            label.setText(country + ": " + num + "avaiable trip");
         }else{
-            australiaLabel.setText("Australia: "+ num + " avaiable trips");
+            label.setText(country + ": " + num + " avaiable trips");
         }
+    }
+    public void australiaMouseEntered(){
+
+        handleMouseEntered("Australia", australiaLabel);
     }
 
     public void indiaMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num = quoteController.numberOfTrps("India");
-        if(num == 1) {
-            indiaLable.setText("India: " + num + " avaiable trip");
-        }else {
-            indiaLable.setText("India: " + num + " avaiable trips");
-        }
+        handleMouseEntered("India", indiaLable);
     }
 
     public void spainMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Spain");
-        if(num == 1){
-            europeLabel.setText("Spain: "+ num + " avaiable trip");
-        }else {
-            europeLabel.setText("Spain: "+ num + " avaiable trips");
-        }
+        handleMouseEntered("Spain", europeLabel);
     }
 
     public void italyMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Italy");
-        if(num == 1) {
-            europeLabel.setText("Italy: " + num + " avaiable trip");
-        }else {
-            europeLabel.setText("Italy: " + num + " avaiable trips");
-        }
+        handleMouseEntered("Italy", europeLabel);
     }
 
     public void brasilMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Brasil");
-        //HomePageGuideMapController homePageGuideController = new HomePageGuideMapController();
-        //int num = homePageGuideController.numberOfTrps("Brasil");
-        if(num == 1){
-            brasilLabel.setText("Brasil: " + num + " avaiable trip");
-        }else {
-            brasilLabel.setText("Brasil: " + num + " avaiable trips");
-        }
+        handleMouseEntered("Brasil", brasilLabel);
     }
 
     public void argentinaMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Argentina");
-        //HomePageGuideMapController homePageGuideController = new HomePageGuideMapController();
-        //int num = homePageGuideController.numberOfTrps("Argentina");
-        if(num == 1){
-            argentinaLabel.setText("Argentina: "+ num + " avaiable trip");
-        }else{
-            argentinaLabel.setText("Argentina: "+ num + " avaiable trips");
-        }
+        handleMouseEntered("Argentina", argentinaLabel);
     }
 
     public void peruMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Perù");
-        //HomePageGuideMapController homePageGuideController = new HomePageGuideMapController();
-        //int num = homePageGuideController.numberOfTrps("Perù");
-        if (num == 1) {
-            peruLabel.setText("Perù: "+ num + " avaiable trip");
-        }else{
-            peruLabel.setText("Perù: "+ num + " avaiable trips");
-        }
+        handleMouseEntered("Perù", peruLabel);
     }
 
     public void chileMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Chile");
-        //HomePageGuideMapController homePageGuideController = new HomePageGuideMapController();
-        //int num = homePageGuideController.numberOfTrps("Chile");
-        if (num == 1){
-            chileLabel.setText("Chile: "+ num + " avaiable trip");
-        }else {
-            chileLabel.setText("Chile: " + num + " avaiable trips");
-        }
+        handleMouseEntered("Chile", chileLabel);
     }
 
     public void usaMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("USA");
-        //HomePageGuideMapController homePageGuideController = new HomePageGuideMapController();
-        //int num = homePageGuideController.numberOfTrps("USA");
-        if(num == 1){
-            europeLabel.setText("USA: "+ num + " avaiable trip");
-        }else{
-            europeLabel.setText("USA: "+ num + " avaiable trips");
-        }
-
+        handleMouseEntered("USA", europeLabel);
     }
 
     public void mexicoMouseEntered(){
 
-        BookTripController quoteController = new BookTripController();
-        int num =  quoteController.numberOfTrps("Mexico");
-        //HomePageGuideMapController homePageGuideController = new HomePageGuideMapController();
-        //int num = homePageGuideController.numberOfTrps("Mexico");
-        if(num == 1){
-            mexicoLabel.setText("Mexico: "+ num + " avaiable trip");
-        }else{
-            mexicoLabel.setText("Mexico: "+ num + " avaiable trips");
-        }
+        handleMouseEntered("Mexico", mexicoLabel);
 
     }
 
@@ -305,146 +195,68 @@ public class HomePageGuideMapGUIController {
     public void canadaMouseExited(){
         mexicoLabel.setText("");}
 
-    public void MouseExited(){
-        mexicoLabel.setText("");}
 
-    public void australiaAction() throws IOException {
-
+    private void loadCategoryView(String country) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
         Parent root = loader.load();
 
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Australia");
+        CategoryGUIController categoryGUIController = loader.getController();
+        categoryGUIController.setCountryLabel(country);
 
         Stage dialog = Main.getStage();
         Scene scene = new Scene(root);
         dialog.setScene(scene);
         dialog.show();
+    }
+
+    public void australiaAction() throws IOException {
+
+        loadCategoryView("Australia");
     }
 
     public void indiaAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("India");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("India");
     }
 
     public void spainAction() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
 
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Spain");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("Spain");
     }
 
     public void italyAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Italy");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("Italy");
     }
 
     public void brasilAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Brasil");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("Brasil");
     }
 
     public void argentinaAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Argentina");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("Argentina");
     }
 
     public void peruAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Perù");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("Perù");
     }
 
     public void chileAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Chile");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("Chile");
     }
 
     public void usaAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("USA");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("USA");
     }
 
     public void mexicoAction() throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/Category.fxml"));
-        Parent root = loader.load();
-
-        CategoryGUIController categoryGUIController  = loader.getController();
-        categoryGUIController.setCountryLabel("Mexico");
-
-        Stage dialog = Main.getStage();
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();
+        loadCategoryView("Mexico");
     }
 
 
@@ -474,26 +286,11 @@ public class HomePageGuideMapGUIController {
     }
 
     public void upcomingAction() throws IOException, NotFoundException {
-        /*Parent root;
-        Stage dialog = Main.getStage();
-
-        root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("UpcomingTrips.fxml")));
-
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        dialog.show();*/
-
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/adventures/UpcomingTrips.fxml"));
         Parent root = loader.load();
 
         UpcomingTripsGUIController upcomingTripsGUIController = loader.getController();
-        //relaxGUIController.setTitle(category);
-
-        //relaxGUIController.setCountry(country);
-
-// qui
-        //upcomingTripsGUIController.initialize();
         upcomingTripsGUIController.inizio();
 
         Stage dialog = Main.getStage();
--- src/main/java/com/example/adventures/graphiccontroller/HomePageTravelerMapGUIController.java
+++ src/main/java/com/example/adventures/graphiccontroller/HomePageTravelerMapGUIController.java
@@ -287,7 +287,4 @@ public class HomePageTravelerMapGUIController{
 
 
     }
-
-
-
 }
