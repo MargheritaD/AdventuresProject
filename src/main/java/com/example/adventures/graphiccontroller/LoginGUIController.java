145876d44d77137b7595cef71270eabce258b687
margheritadi <margherita.di@edu.uah.es>
2024-07-07 17:01:11 +0200

    11111

--- src/main/java/com/example/adventures/graphiccontroller/LoginGUIController.java
+++ src/main/java/com/example/adventures/graphiccontroller/LoginGUIController.java
@@ -28,25 +28,13 @@ public class LoginGUIController {
 
     public void switchToWelcome() throws IOException, NotFoundException {
 
-        System.out.println("PROVA LOGIN STEP 1");
 
         try {
 
-            String username = usernameText.getText();
-            String password = passwordText.getText();
-            System.out.println("Username: " + username);
-            System.out.println("Password: " + password);
-
-            //System.out.println("prima creazione controller");
             LoginBean loginBean = new LoginBean(usernameText.getText(),passwordText.getText());
             LoginController loginController = new LoginController();
 
-            //System.out.println("PROVA LOGIN STEP 2");
-
             loginController.checkUser(loginBean);
-
-            //System.out.println("PROVA LOGIN STEP 3");
-            //System.out.println(loginBean.getRole() + " RUOLO");
 
             Parent root;
             Stage dialog = Main.getStage();
@@ -69,58 +57,11 @@ public class LoginGUIController {
             dialog.show();
 
         } catch (EmailFormatException | UserNotFoundException  e) {
-            //ShowExceptionSupport.showException(e.getMessage());
+            Printer.printError(e.getMessage());
         } catch (IOException e){
             Printer.printError(e.getMessage());
         }
-        /*
 
-        System.out.println("PROVA LOGIN STEP 1");
-
-        try{
-            System.out.println("PROVA LOGIN TRY");
-
-            String username = usernameText.getText();
-            String password = passwordText.getText();
-            System.out.println("Username: " + username);
-            System.out.println("Password: " + password);
-
-            LoginBean loginBean = new LoginBean(usernameText.getText(), passwordText.getText());
-            LoginController loginController = new LoginController();
-
-            System.out.println("PROVA LOGIN STEP 2");
-
-            loginController.checkUser(loginBean);
-
-            System.out.println("PROVA LOGIN STEP 3");
-            System.out.println(loginBean.getRole() + " RUOLO");
-
-            Parent root;
-            Stage dialog = Main.getStage();
-
-            switch (loginBean.getRole()){
-                case 1 -> {
-                    System.out.println("GUIDAAAA");
-                    loginController.guideLogin(loginBean);
-                    root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("AccessPage2.fxml")));
-                }
-                case 2 -> {
-                    System.out.println("VIAGGIATORE");
-                    loginController.travelerLogin(loginBean);
-                    root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("homePageTraveler.fxml")));
-                }
-                default -> throw new UserNotFoundException();
-            }
-
-            Scene scene = new Scene(root);
-            dialog.setScene(scene);
-            dialog.show();
-        }catch (EmailFormatException | UserNotFoundException e){
-            //ShowExceptionSupport.showException(e.getMessage());
-        }catch (IOException e){
-            Printer.printError(e.getMessage());
-        }
-        */
     }
 
     public void switchToNew() throws IOException{
