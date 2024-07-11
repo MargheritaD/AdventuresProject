c7a0731e90f0ae7c955a2e5cf9abcaff79249f28
margheritadi <margherita.di@edu.uah.es>
2024-07-11 17:54:06 +0200

    Sonar cloud: Modifica CLIHomeTraveler

--- src/main/java/com/example/adventures/cli/CLIHomeTraveler.java
+++ src/main/java/com/example/adventures/cli/CLIHomeTraveler.java
@@ -12,63 +12,27 @@ import java.util.List;
 public class CLIHomeTraveler extends AbstractCLI {
 
     public void start() {
-
-        boolean choose = true;
+        boolean chooseT = true;
         CountryCategoryBean countryCategoryBean = new CountryCategoryBean();
         CLITravelerRequests travelerRequests = new CLITravelerRequests();
 
-        while(choose){
+        while (chooseT) {
             int choice;
-            try{
+            try {
                 choice = showMenu();
-                switch (choice){
-                    case 1 -> { //Choose country:
-                        choose = false;
-                        int country;
-                        country = chooseCountry();
-
-                        switch (country){ // choose category
-                            case 1 -> {
-
-                                countryCategoryBean.setCountry("Italy");
-                                int category = chooseCategory();
-                                setCategory(category, countryCategoryBean);
-                            }
-                            case 2 -> {
-
-                                countryCategoryBean.setCountry("Argentina");
-                                int category = chooseCategory();
-                                setCategory(category, countryCategoryBean);
-                            }
-                            case 3 -> {
-
-                                countryCategoryBean.setCountry("USA");
-                                int category = chooseCategory();
-                                setCategory(category, countryCategoryBean);
-                            }
-                            case 4 -> {
-
-                                countryCategoryBean.setCountry("Mexico");
-                                int category = chooseCategory();
-                                setCategory(category, countryCategoryBean);
-                            }
-                        }
+                switch (choice) {
+                    case 1 -> { // Choose country:
+                        chooseT = false;
+                        handleCountrySelection(countryCategoryBean);
                     }
-                    case 2 -> { // request
+                    case 2 ->  // Your requests
+                            travelerRequests.start();
 
-                        //choose = false;
-                        //CLIPrinter.printMessage("Not implemented");
-                        travelerRequests.start();
-                    }
-                    case 3 -> {
-                        choose = false;
-                        logout();
-
-                    }
-                    case 4 -> {
-                        choose = false;
+                    case 3 -> { // logout
+                        chooseT = false;
                         System.exit(0);
                     }
+                    default -> CLIPrinter.printMessage("Invalid choice");
                 }
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -76,99 +40,75 @@ public class CLIHomeTraveler extends AbstractCLI {
         }
     }
 
-    private static int showMenu(){
+    private void handleCountrySelection(CountryCategoryBean countryCategoryBean) throws SQLException, NotFoundException {
+        int countryT = chooseCountry();
+        switch (countryT) {
+            case 1 -> countryCategoryBean.setCountry("Italy");
+            case 2 -> countryCategoryBean.setCountry("Argentina");
+            case 3 -> countryCategoryBean.setCountry("USA");
+            case 4 -> countryCategoryBean.setCountry("Mexico");
+            default -> {
+                CLIPrinter.printMessage("Invalid country choice");
+                return;
+            }
+        }
+        int category = chooseCategory();
+        setCategory(category, countryCategoryBean);
+    }
 
+    private int showMenu() {
         CLIPrinter.printMessage("\nMenu: \n");
         CLIPrinter.printMessage("\n1) Choose country: \n");
         CLIPrinter.printMessage("2) Your requests: \n");
         CLIPrinter.printMessage("3) Logout: \n");
-        CLIPrinter.printMessage("4) Quit: \n\n");
-
-        return  getMenuChoice(1,4);
+        return getMenuChoice(1, 4);
     }
 
-    private static int chooseCountry(){
-
-        CLIPrinter.printMessage("\n********************************* Country list ********************************* \n\n");
+    private int chooseCountry() {
+        CLIPrinter.printMessage("\n****************************** Country list ****************************** \n\n");
         CLIPrinter.printMessage("1. Italy: \n");
         CLIPrinter.printMessage("2. Argentina: \n");
         CLIPrinter.printMessage("3. USA: \n");
         CLIPrinter.printMessage("4. Mexico: \n\n");
-        // aggiungi altre country
-
-        return  getMenuChoice(1,4);
+        return getMenuChoice(1, 4);
     }
 
-    private static int chooseCategory(){
-
-        CLIPrinter.printMessage("\n********************************* Category list ********************************* \n\n");
+    private int chooseCategory() {
+        CLIPrinter.printMessage("\n****************************** Category list ****************************** \n\n");
         CLIPrinter.printMessage("1. Relax: \n");
         CLIPrinter.printMessage("2. Sport: \n");
         CLIPrinter.printMessage("3. Dog trekking: \n");
         CLIPrinter.printMessage("4. Safari: \n");
         CLIPrinter.printMessage("5. Food tasting: \n");
         CLIPrinter.printMessage("6. Fun: \n\n");
-
-        return  getMenuChoice(1,6);
+        return getMenuChoice(1, 6);
     }
 
-    public static void setCategory(int category, CountryCategoryBean countryCategoryBean) throws SQLException, NotFoundException {
-        switch (category){
-            case 1 ->{
-                countryCategoryBean.setCategory("Relax");
-                BookTripController bookTripController = new BookTripController();
-                List<TripBean> listOfTripBean = bookTripController.selectCountryAndCategory(countryCategoryBean);
-                CLIListTripCategoryCountry cliListTripCategoryCountry = new CLIListTripCategoryCountry();
-                cliListTripCategoryCountry.start(listOfTripBean);
-                //System.out.println(countryCategoryBean.getCategory());
-                //System.out.println(countryCategoryBean.getCountry());
-            }
+    public void setCategory(int category, CountryCategoryBean countryCategoryBean) throws SQLException, NotFoundException {
 
-            case 2 ->{
-                countryCategoryBean.setCategory("Sport");
-                BookTripController bookTripController = new BookTripController();
-                List<TripBean> listOfTripBean = bookTripController.selectCountryAndCategory(countryCategoryBean);
-                CLIListTripCategoryCountry cliListTripCategoryCountry = new CLIListTripCategoryCountry();
-                cliListTripCategoryCountry.start(listOfTripBean);
-                System.out.println(countryCategoryBean.getCategory());
-                System.out.println(countryCategoryBean.getCountry());
-            }
-            case 3 ->{
-                countryCategoryBean.setCategory("Dog trekking");
-                BookTripController bookTripController = new BookTripController();
-                List<TripBean> listOfTripBean = bookTripController.selectCountryAndCategory(countryCategoryBean);
-                CLIListTripCategoryCountry cliListTripCategoryCountry = new CLIListTripCategoryCountry();
-                cliListTripCategoryCountry.start(listOfTripBean);
-                System.out.println(countryCategoryBean.getCategory());
-                System.out.println(countryCategoryBean.getCountry());
-            }
-            case 4 ->{
-                countryCategoryBean.setCategory("Safari");
-                BookTripController bookTripController = new BookTripController();
-                List<TripBean> listOfTripBean = bookTripController.selectCountryAndCategory(countryCategoryBean);
-                CLIListTripCategoryCountry cliListTripCategoryCountry = new CLIListTripCategoryCountry();
-                cliListTripCategoryCountry.start(listOfTripBean);
-                System.out.println(countryCategoryBean.getCategory());
-                System.out.println(countryCategoryBean.getCountry());
-            }
-            case 5 ->{
-                countryCategoryBean.setCategory("Food tasting");
-                BookTripController bookTripController = new BookTripController();
-                List<TripBean> listOfTripBean = bookTripController.selectCountryAndCategory(countryCategoryBean);
-                CLIListTripCategoryCountry cliListTripCategoryCountry = new CLIListTripCategoryCountry();
-                cliListTripCategoryCountry.start(listOfTripBean);
-                System.out.println(countryCategoryBean.getCategory());
-                System.out.println(countryCategoryBean.getCountry());
-            }
-            case 6 ->{
-                countryCategoryBean.setCategory("Fun");
-                BookTripController bookTripController = new BookTripController();
-                List<TripBean> listOfTripBean = bookTripController.selectCountryAndCategory(countryCategoryBean);
-                CLIListTripCategoryCountry cliListTripCategoryCountry = new CLIListTripCategoryCountry();
-                cliListTripCategoryCountry.start(listOfTripBean);
-                System.out.println(countryCategoryBean.getCategory());
-                System.out.println(countryCategoryBean.getCountry());
-            }
+        String categoryName = getCategoryName(category);
+        if (categoryName != null) {
+
+            countryCategoryBean.setCategory(categoryName);
+            BookTripController bookTripController = new BookTripController();
+            List<TripBean> listOfTripBean = bookTripController.selectCountryAndCategory(countryCategoryBean);
+
+            CLIListTripCategoryCountry cliListTripCategoryCountry = new CLIListTripCategoryCountry();
+            cliListTripCategoryCountry.start(listOfTripBean);
+        } else {
+            CLIPrinter.printMessage("Invalid category choice");
         }
+    }
+
+    private String getCategoryName(int category) {
+        return switch (category) {
+            case 1 -> "Relax";
+            case 2 -> "Sport";
+            case 3 -> "Dog trekking";
+            case 4 -> "Safari";
+            case 5 -> "Food tasting";
+            case 6 -> "Fun";
+            default -> null;
+        };
     }
 }
