596b066d8e921bd41c7d68830b76833f3cc3d7e5
margheritadi <margherita.di@edu.uah.es>
2024-07-27 10:47:56 +0200

    Sonar Cloud: exc

--- src/main/java/com/example/adventures/exception/EmailFormatException.java
+++ src/main/java/com/example/adventures/exception/EmailFormatException.java
@@ -2,6 +2,6 @@ package com.example.adventures.exception;
 
 public class EmailFormatException extends Exception {
     public EmailFormatException(String email) {
-            super(" ' " + email +  "' \n email not valid, lease insert a new email");
+            super(" ' " + email +  "' \n email not valid, please insert a new email");
     }
 }
