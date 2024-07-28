a5a7066bcf97c2990f98b5071fc8dc82889fecba
margheritadi <margherita.di@edu.uah.es>
2024-07-28 17:37:17 +0200

    Commit book

--- src/main/java/com/example/adventures/appcontroller/BookTripController.java
+++ src/main/java/com/example/adventures/appcontroller/BookTripController.java
@@ -1,67 +1,15 @@
 package com.example.adventures.appcontroller;
 
 import com.example.adventures.bean.*;
-import com.example.adventures.dao.ItineraryStopDAO;
 import com.example.adventures.dao.RequestDAO;
 import com.example.adventures.dao.TripDAO;
-import com.example.adventures.exception.NotFoundException;
-import com.example.adventures.model.ItineraryStop;
 import com.example.adventures.model.Request;
-import com.example.adventures.model.Trip;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookTripController {
-/*
-    public int numberOfTrps(String country){
-
-        return TripDAO.getNumberTripsByCountry(country);
-    }
-
-    public boolean bell(GuideBean guideBean){
-
-        return RequestDAO.setBell(guideBean);
-    }
-
-    public List<TripBean> selectCountryAndCategory(CountryCategoryBean countryCategoryBean){
-
-        List<Trip> trips = TripDAO.retrieveTripListByCategoryAndCountry(countryCategoryBean.getCategory(), countryCategoryBean.getCountry());
-
-        List<TripBean> tripBeans = new ArrayList<>();
-        for (Trip trip : trips) {
-            TripBean tripBean = new TripBean(trip.getIdTrip(), trip.getTripName(), trip.getOutboundDate(), trip.getReturnDate(), trip.getDepartureCity(), trip.getGuide());
-            tripBeans.add(tripBean);
-        }
-
-        return tripBeans;
-    }
-
-    public List<ItineraryStopBean> tableItinerary(TripBean tripBean){
-
-        List<ItineraryStop> stops = ItineraryStopDAO.retrieveStopList(tripBean.getIdTrip());
-
-        List<ItineraryStopBean> itineraryStopBeans = new ArrayList<>();
-        for (ItineraryStop itineraryStop : stops) {
-            ItineraryStopBean itineraryStopBean = new ItineraryStopBean(itineraryStop.getCity(), itineraryStop.getArrival(), itineraryStop.getDeparture());
-            itineraryStopBeans.add(itineraryStopBean);
-        }
-
-        return itineraryStopBeans;
-    }
-
-    public TripBean getTripDetails(TripBean partialTripBean) throws NotFoundException {
-        int tripId = partialTripBean.getIdTrip();
-
-        // Recupera i dettagli completi del viaggio dal DAO
-        Trip trip = TripDAO.retrieveTripById(tripId);
-        if (trip == null) {
-            throw new NotFoundException("Trip not found for ID: " + tripId);
-        }
-
-        return new TripBean(partialTripBean.getIdTrip(), trip.getTripName(), trip.getDepartureCity(), trip.getCategory(), trip.getOutboundDate(), trip.getReturnDate(), trip.getPrice(), trip.getGuide(), trip.getCountry());
-    }
-*/
     public void sendRequest(RequestBean requestBean){
 
         Request request = new Request(requestBean.getIdTrip(), requestBean.getIdTraveler());
